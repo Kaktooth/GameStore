93678ccca4110cd1c696763244c3621c30c29224
Volodimir Bezridniy <bezridniyv0@gmail.com>
2023-02-17 15:28:20 +0200

    refactor to multi module project

---
+++ recommendation-api-core/src/test/java/com/store/gamestore/RecommendationApiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationApiCoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
